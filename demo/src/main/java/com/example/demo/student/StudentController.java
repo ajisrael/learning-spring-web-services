68f6d4a3de0523aea1e4804a7649467776175f22
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-02-23 13:40:25 -0500

    f   - added dependency injection

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -12,7 +12,8 @@ import java.util.List;
 public class StudentController {
 
     private final StudentService studentService;
-    
+
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
