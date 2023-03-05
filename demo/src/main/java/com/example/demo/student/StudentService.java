2dbbbd695f9fd01d19644b38963cdc6a84336125
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-03-05 13:56:38 -0500

    f   - integrated postgres db with spring app

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    
+
     public List<Student> getStudents() {
         return List.of(
                 new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
