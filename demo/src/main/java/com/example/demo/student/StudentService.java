38dfa5d7d83c1a8d88d829e3ddc0e35222944eb5
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-03-09 02:29:59 -0500

    f   - added put endpoint, logic, and connection to database to update student name and/or email

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,10 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -24,13 +24,39 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        throwExceptionIfEmailAlreadyExists(student.getEmail());
+
+        studentRepository.save(student);
+    }
+
+    private void throwExceptionIfEmailAlreadyExists(String email) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
 
         if (studentOptional.isPresent()) {
             throw new IllegalStateException("email already taken");
         }
+    }
 
-        studentRepository.save(student);
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"
+                ));
+
+        if (propertyIsNotNullOrEmptyAndIsDifferentThanCurrentValue(name, student.getName())) {
+            student.setName(name);
+        }
+
+        if (propertyIsNotNullOrEmptyAndIsDifferentThanCurrentValue(email, student.getEmail())) {
+            throwExceptionIfEmailAlreadyExists(email);
+            student.setEmail(email);
+        }
+
+    }
+
+    private boolean propertyIsNotNullOrEmptyAndIsDifferentThanCurrentValue(String property, String currentValue) {
+        return property != null && property.length() > 0 && !Objects.equals(currentValue, property);
     }
 
     public void deleteStudent(Long studentId) {
@@ -42,4 +68,5 @@ public class StudentService {
 
         studentRepository.deleteById(studentId);
     }
+
 }
