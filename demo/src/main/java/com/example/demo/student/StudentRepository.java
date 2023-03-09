962c8ddeb805d94a107dcaa57f3b4f98b40df1b3
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-03-09 01:42:19 -0500

    f   - tied post request to db and added validation logic

--- demo/src/main/java/com/example/demo/student/StudentRepository.java
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,9 +3,16 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // T -> Student : object being worked with for repository
 // id -> Long : the type of object for the id of the reference object T
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // The name of the method is used to generate sql to run on invocation of said method.
+    // You can also add the following annotation to do the same thing:
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
